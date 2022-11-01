275e85a937b25bc5d74750d6388cbc2a135bee17
Tesh2714 <teas38mum@gmail.com>
2022-11-01 14:57:19 -0700

    0-sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" Function that queries the Reddit API """
+"""
+  function that queries the Reddit API
+  and returns the number of subscribers
+"""
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """  Args:
-         subreddit: subreddit name
-    Returns:
-         number of subscribers to the subreddit,
-         or 0 if subreddit requested is invalid"""
-    headers = {'User-Agent': 'xica369'}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
-    if response.status_code == 200:
-        return (response.json().get("data").get("subscribers"))
-    return (0)
+    """initializate"""
+    if (type(subreddit) is not str):
+        return(0)
+    url_api = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
+    headers = {'user-agent': 'safari:holberton/0.1.0'}
+    response = requests.get(url_api, headers=headers)
+    if response.status_code is not 200:
+        return(0)
+    return(response.json().get("data").get("subscribers"))
